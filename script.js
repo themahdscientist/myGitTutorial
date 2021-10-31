4c6da68cd17d50e2bd863042e40ee5427c8ce5e8
Ifeanyichukwu <ifeanyichukwuakudinobi86@gmail.com>
2021-10-31 01:12:18 +0100

    Configured express server

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 2000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!!`))
+app.get('/', (req, res) => res.send(`Wow this actually worked on port ${port}!!`))
+app.listen(port, () => console.log(`Portfolio server listening at port ${port}!`))
