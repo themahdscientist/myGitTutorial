867ed847ed056774852a3defb5f23c88c505b1d3
Ifeanyichukwu <ifeanyichukwuakudinobi86@gmail.com>
2021-11-30 13:38:54 +0100

    continuation

--- enquire/script/index.js
+++ enquire/script/index.js
@@ -1,6 +1,9 @@
 const nav = document.getElementById('home_nav');
 const ham = document.getElementById('home_ham');
+const main = document.getElementById('home_main_items');
 
+ham.addEventListener('click', nav_opener);
 
-
-
+function nav_opener() {
+    nav.classList.toggle('nav');
+}
