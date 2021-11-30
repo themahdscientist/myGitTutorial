397c4305c25a8c5534fb9b17d1268ef40328ed3e
Ifeanyichukwu <ifeanyichukwuakudinobi86@gmail.com>
2021-11-30 21:59:31 +0100

    finished with the menu and hamburger functionality

--- enquire/script/index.js
+++ enquire/script/index.js
@@ -1,9 +1,22 @@
-const nav = document.getElementById('home_nav');
-const ham = document.getElementById('home_ham');
-const main = document.getElementById('home_main_items');
+const nav = document.getElementById("home_nav");
+const ham = document.getElementById("ham_img");
+const main = document.getElementById("home_main_items");
+const close = document.getElementById("close_img");
 
-ham.addEventListener('click', nav_opener);
+ham.addEventListener("click", nav_opener);
+close.addEventListener("click", nav_closer);
+
+main.classList.add("no_display");
+close.classList.add("no_display");
 
 function nav_opener() {
-    main.classList.toggle('home_main');
+  main.classList.toggle("no_display");
+  close.classList.toggle("no_display");
+  ham.classList.toggle("no_display");
+}
+
+function nav_closer() {
+  main.classList.toggle("no_display");
+  close.classList.toggle("no_display");
+  ham.classList.toggle("no_display");
 }
