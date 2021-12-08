a6ca5775dc5f7a810cf9528d75a88086cfae0b69
Ifeanyichukwu <ifeanyichukwuakudinobi86@gmail.com>
2021-12-08 22:52:45 +0100

    changed the background of the index.html file

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// const express = require('express');
-// const app = express();
-// const port = 3000;
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// app.get('/', (req, res) => res.send(`Wow this actually worked on port ${port}!!`));
-// app.listen(port, () => console.log(`Portfolio server listening at port ${port}!`));
-
+app.get('/', (req, res) => res.send(`Wow this actually worked on port ${port}!!`));
+app.listen(port, () => console.log(`Portfolio server listening at port ${port}!`));
