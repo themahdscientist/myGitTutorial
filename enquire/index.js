65f097d321a27e66b81261923040d343be5715eb
Ifeanyichukwu <ifeanyichukwuakudinobi86@gmail.com>
2021-12-18 03:25:48 +0100

    finished with the menu

--- enquire/index.js
+++ enquire/index.js
@@ -1,7 +1,18 @@
 function main() {
-    document.getElementById('menu').addEventListener('click', function () {
-        alert('hello')
-    })
+  const menu = document.getElementById("menu_wrap");
+  const close = document.getElementById("close_wrap");
+  const closeBtn = document.getElementById("close");
+  menu.addEventListener("click", function () {
+    document.getElementById("home_nav").style.left = "0";
+    menu.style.display = "none";
+    closeBtn.classList.add("active");
+  });
+
+  close.addEventListener("click", function () {
+    document.getElementById("home_nav").style.left = "-500px";
+    menu.style.display = "block";
+    closeBtn.classList.remove("active");
+  });
 }
 
 main();
